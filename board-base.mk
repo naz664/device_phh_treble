caad1ea768d39f4dff40bc80964bcece651dbe46
naz664 <nazimnavas145@gmail.com>
2023-01-15 06:38:24 +0000

    Disable shared blocks for now Later, We can use it according to our preference

--- board-base.mk
+++ board-base.mk
@@ -10,6 +10,7 @@ BOARD_ROOT_EXTRA_FOLDERS += bt_firmware sec_storage efs persist
 BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
 
 BUILD_BROKEN_DUP_RULES := true
+#BOARD_EXT4_SHARE_DUP_BLOCKS := true
 TARGET_NO_KERNEL_OVERRIDE := true
 
 BOARD_ROOT_EXTRA_SYMLINKS := $(filter-out $(BOARD_ROOT_EXTRA_SYMLINKS),/mnt/vendor/persist:/persist)
