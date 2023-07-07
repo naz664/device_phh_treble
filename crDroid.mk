b927c8f7dd1268ae6f9a828fe2e20326e2d541ae
naz664 <nazimnavas145@gmail.com>
2023-07-07 14:02:25 +0000

    treble: Import crDroid makefile

---
+++ crDroid.mk
@@ -0,0 +1,20 @@
+$(call inherit-product, vendor/lineage/config/common.mk)
+$(call inherit-product, vendor/lineage/config/common_full.mk)
+$(call inherit-product, vendor/lineage/config/BoardConfigSoong.mk)
+$(call inherit-product, vendor/lineage/config/BoardConfigLineage.mk)
+$(call inherit-product, device/lineage/sepolicy/common/sepolicy.mk)
+-include vendor/lineage/build/core/config.mk
+TARGET_NO_KERNEL_OVERRIDE := true
+TARGET_NO_KERNEL_IMAGE := true
+SELINUX_IGNORE_NEVERALLOWS := true
+
+TARGET_FACE_UNLOCK_SUPPORTED := true
+
+# PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+#    ro.system.ota.json_url=https://raw.githubusercontent.com/naz664/crDroid_gsi/14/ota.json
+
+# Set Bootanimation at 720P
+TARGET_BOOT_ANIMATION_RES := 720
+
+# APN
+PRODUCT_PACKAGES += apns-conf.xml
