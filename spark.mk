2a1abbbb3b64774fa3c1d8652361d406fae211c2
naz664 <nazimnavas145@gmail.com>
2023-09-23 18:02:33 +0530

    treble: Import Spark makefile
    Signed-off-by: naz664 <nazimnavas145@gmail.com>

---
+++ spark.mk
@@ -0,0 +1,16 @@
+$(call inherit-product, vendor/spark/config/common.mk)
+$(call inherit-product, vendor/spark/config/common_full.mk)
+$(call inherit-product, vendor/spark/config/BoardConfigSoong.mk)
+$(call inherit-product, vendor/spark/config/BoardConfigSpark.mk)
+$(call inherit-product, device/spark/sepolicy/common/sepolicy.mk)
+$(call inherit-product, vendor/spark/config/bootanimation.mk)
+-include vendor/spark/build/core/config.mk
+TARGET_NO_KERNEL_OVERRIDE := true
+TARGET_NO_KERNEL_IMAGE := true
+SELINUX_IGNORE_NEVERALLOWS := true
+
+# Set Bootanimation at 720P
+TARGET_BOOT_ANIMATION_RES := 720
+
+# APN
+PRODUCT_PACKAGES += apns-conf.xml
